4f5dd24ad3fb0886658c530d2f430ea7665f849c
troy <troy@fb8b9dc4-0462-4bc1-b9b3-261ab0a2bc9c>
2011-02-07 15:25:55 +0000

    Testando, ja que deu erro no anterior

---
+++ Assets/Project/Scripts/Funcionario/Atributos.js
@@ -0,0 +1,35 @@
+class Atributos extends System.ValueType{
+
+//fields
+public var adaptabilidade : int;
+public var autoDitada : int;
+public var detalhista : int;
+public var negociacao : int;
+public var objetividade : int;
+public var organizacao : int;
+public var paciencia : int;
+public var raciocinioLogico : int;
+public var relacionamentoHumano : int;
+public var papel : String;
+public var salario : int;
+public var nummesa : int;
+
+//Constructor 
+
+	public function Atributos(a1 : int, a2 : int, a3 : int, a4 : int, a5 : int, a6 : int, a7 : int, a8 : int, a9 : int, a10 : String, a11 : int, a12 : int )
+	{
+		this.adaptabilidade = a1;
+		this.autoDitada = a2;
+		this.detalhista = a3;
+		this.negociacao = a4;
+		this.objetividade = a5;
+		this.organizacao = a6;
+		this.paciencia = a7;
+		this.raciocinioLogico =a8;
+		this.relacionamentoHumano = a9;
+		this.papel = a10;
+		this.nummesa = a11;
+		this.salario = a12;
+		
+	}
+}
---
+++ Assets/Project/Scripts/Player/PlayerStats.js
@@ -0,0 +1,13 @@
+
+public var saldo = 0;
+
+function Update () {
+}
+
+function GetSaldo() {
+	return saldo;
+}
+
+function SetSaldo(t: int) {
+	saldo = t;
+}
